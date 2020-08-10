3981d4e12c1f3dd20cd155ad884ab7d3182f0c27
shubhamgupta <guptashubham1798@gmail.com>
2020-08-10 15:33:09 +0530

    conn file

---
+++ connect_mongo_data/Conn_String.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace connect_mongo_data
+{
+    public class Conn_String
+    {
+        public String mongo_conn; 
+        public Conn_String()
+        {
+            mongo_conn="mongodb://localhost:27017";
+        }
+    }
+}
